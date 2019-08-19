a0da075ea67fd2eebeb46cb27fcc6723ac091c9a
jcfields0103 <jcfields0103@users.noreply.github.com>
2019-08-19 12:10:21 -0400

    Initial commit

---
+++ BugTracker2/Models/TicketAttachment.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace BugTracker2.Models
+{
+    public class TicketAttachment
+    {
+        public int Id { get; set; }
+        public int TicketId { get; set; }
+        public int UserId { get; set; }
+        public string Title { get; set; }
+        public string Description { get; set; }
+        public string AttachmentUrl { get; set; }
+        public DateTime Created {get;set;}
+
+        //nav
+        public virtual Ticket Ticket { get; set; }
+        public virtual ApplicationUser User { get; set; }
+
+    }
+}
